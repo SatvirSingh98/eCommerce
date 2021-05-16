cd55a9274d9cc1955687a3a086c060eed4e0af6b
satvir <satvirsingh.juc@gmail.com>
2021-05-16 15:41:02 +0530

    added robust reverse urls for products section

--- src/products/models.py
+++ src/products/models.py
@@ -2,6 +2,7 @@ import os
 
 from django.db import models
 from django.db.models.signals import pre_save
+from django.urls import reverse
 
 from .utils import unique_slug_generator
 
@@ -68,6 +69,9 @@ class Product(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('products:detail', kwargs={'slug': self.slug})
 
 
 def product_pre_save_reciever(sender, instance, *args, **kwargs):
