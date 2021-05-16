ee5cea9c8d7237077238c9153e1998e352b5397e
satvir <satvirsingh.juc@gmail.com>
2021-05-16 15:53:12 +0530

    Made seperate apps folder for django apps

--- src/eCommerce/settings.py
+++ src/eCommerce/settings.py
@@ -28,7 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # our apps
-    'products',
+    'apps.products',
 
     # third-party apps
     'crispy_forms',
@@ -93,20 +93,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': ('django.contrib.auth.password_validation.'
-                 'UserAttributeSimilarityValidator'),
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
-        'NAME': ('django.contrib.auth.password_validation.'
-                 'MinimumLengthValidator'),
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     {
-        'NAME': ('django.contrib.auth.password_validation.'
-                 'CommonPasswordValidator'),
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
-        'NAME': ('django.contrib.auth.password_validation.'
-                 'NumericPasswordValidator'),
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
 
--- src/eCommerce/urls.py
+++ src/eCommerce/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     path('login/', login_page, name='login'),
     path('logout/', logout_page, name='logout'),
     path('register/', register_page, name='register'),
-    path('products/', include('products.urls')),
+    path('products/', include('apps.products.urls')),
     path('admin/', admin.site.urls),
 ]
 
